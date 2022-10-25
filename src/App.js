beff1a14f64a1e981ff55acddb5d730ea1e81bc5
pr9272944 <116561473+pr9272944@users.noreply.github.com>
2022-10-25 08:12:01 -0300

    2
    2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Start Trading->Today
+          Start Play->Today
         </a>
       </header>
     </div>
